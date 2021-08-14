d24370125df854c54eb309a013cba4b4b649651b
Crystal Odi <crystalodi@gmail.com>
2021-08-14 16:19:57 -0500

    Remove App styling, change id to string, add link to page for testing

--- src/components/PrizeCard.vue
+++ src/components/PrizeCard.vue
@@ -1,9 +1,11 @@
 <template>
-    <div class="col-xl-4 col-md-2 col-12">
+    <div class="col-xl-4 col-md-6 col-12">
         <div class="card prize-card">
             <img class="card-img-top" v-bind:src="prize.image_url" v-if="prize.image_url"/>
             <div class="card-body">
-                <div class="card-title">Win a {{prize.name}}</div>
+                <div class="card-title">Win a {{prize.name}}
+                </div>
+                <router-link :to="`/prize/${prize.id}`" tag="a" class="card-link">View Details</router-link>
             </div>
         </div>
     </div>
--- src/fakeData.js
+++ src/fakeData.js
@@ -1,41 +1,41 @@
 export const prizes = [
     {
-        id: 1,
+        id: "1",
         name: "TV",
         description: "A big flat screen TV",
         image_url: "https://i.imgur.com/ok2DKYz.jpg",
         quantity: 99
     },
     {
-        id: 2,
+        id: "2",
         name: "Ipad",
         description: "A brand new rose gold tablet",
         image_url: "https://i.imgur.com/Ozes0Y1.jpg",
         quantity: 99
     },
     {
-        id: 3,
+        id: "3",
         name: "Bitcoin",
         description: "A bitcoin worth thousands",
         image_url: "https://i.imgur.com/SSGp5MZ.jpg",
         quantity: 99
     },
     {
-        id: 4,
+        id: "3",
         name: "Macbook Pro",
         description: "A fancy slim laptop",
         image_url: "https://i.imgur.com/2aE6fmO.jpg",
         quantity: 99
     },
     {
-        id: 5,
+        id: "3",
         name: "Headphones",
         description: "Fancy and stylish noise cancelling headphones",
         image_url: "https://i.imgur.com/6dfg2fk.jpg",
         quantity: 99
     },
     {
-        id: 6,
+        id: "3",
         name: "Block of Gold",
         description: "A block of gold worth a lot of money",
         image_url: "https://i.imgur.com/8AWzqWA.jpg",
